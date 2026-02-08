aa5ec7fab28c5ca22064a62da0cda9152af36f14
Ezedin Ahmed <ezedinadefa@gmail.com>
2026-02-08 16:13:06 +0300

    implementing sign up page

--- src/frontend/src/components/Home.vue
+++ src/frontend/src/components/Home.vue
@@ -4,6 +4,7 @@ import Login from './Login.vue';
 import featureBook from './featureBook.vue';
 import Footer from './footer.vue';
 import catagories from './catagories.vue';
+import signUp from './signUp.vue';
 </script>
 <template>
 <Search></Search>
---
+++ src/frontend/src/components/signUp.vue
@@ -0,0 +1,53 @@
+<script setup>
+    import { ref} from 'vue'
+    import { EyeIcon, EyeSlashIcon } from '@heroicons/vue/24/outline'
+
+    const showPassword = ref(false)
+</script>
+
+<template>
+    <div class="grid grid-cols-[2fr_3fr] gap-3 min-h-screen w-full">
+        <div class="flex flex-col gap-3 ml-20 mt-8">
+            <h1 class="text-[35px] font-bold font-sans">Sign Up</h1>
+            <p class="text-[17px]  mb-2 text-[#999797]">Sign up to enjoy the feature of e-book</p>
+            <div class="flex flex-col gap-4">
+                <div class="relative">
+                    <input  type="text" placeholder="" class="w-5/6 px-4 py-3 border-2 border-gray-200 rounded-lg focus:outline-none focues:border-blue">
+                <span class="absolute -top-3 left-4 bg-white px-2 text-gray-500 text-sm">Your Name</span>
+                </div>
+                <div class="relative">
+                    <input  type="text" placeholder="" class="w-5/6 px-4 py-3 border-2 border-gray-200 rounded-lg focus:outline-none focues:border-blue">
+                <span class="absolute -top-3 left-4 bg-white px-2 text-gray-500 text-sm">User Name</span>
+                </div>
+                <div class="relative">
+                    <input  type="text" placeholder="" class="w-5/6 px-4 py-3 border-2 border-gray-200 rounded-lg focus:outline-none focues:border-blue">
+                <span class="absolute -top-3 left-4 bg-white px-2 text-gray-500 text-sm">Email</span>
+                </div>
+                <div class="relative">
+                    <input  type="text" placeholder="Password" class=" w-5/6 px-4 py-3 border-2 border-gray-200 rounded-lg focus:outline-none placeholder:text-gray-500 absolute focues:border-blue">
+                    <button type="button" @click="showPassword = !showPassword" class=" top-3 right-18">
+                        <EyeIcon v-if="!showPassword" class="w-5 h-5 text-gray-500"/>
+                        <EyeSlashIcon v-if="showPassword" class="w-5 h-5 text-gray-500" />
+                    </button>
+                </div>
+            </div>
+            <div>
+                
+            </div>
+            <button class="w-5/6 mt-8 h-12 rounded-lg text-white font-bold text-[17px] bg-blue-600"> Sign Up</button>
+            <span class="text-blue-800  w-5/6 underline text-center underline-offset-auto cursor-pointer">Forget password?</span>
+            <div class="relative w-5/6">
+            <hr class=" mt-3 border-[#a7a4a4]">
+            <span class="absolute w-5 h-5 px-1 bg-white -top-0.5 left-1/2">or</span>
+            </div>
+            
+            <div class="flex justify-center w-5/6 mt-3">
+                <p class="text-[#918e8e]">Already have an account?? <span class="text-blue-800 underline underline-offset-auto cursor-pointer">Sign in</span></p>
+            </div>
+        </div>
+        <div class="h-full">
+            <img class="h-[100vh]" src="\src\assets\stockof_books.jpg" alt="">
+        </div>
+            
+    </div>
+</template>
