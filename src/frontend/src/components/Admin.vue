2031523e9d98f4160f159aef4528515b984245ef
Ezedin Ahmed <ezedinadefa@gmail.com>
2026-02-14 12:42:29 +0300

    listing books in admin page

--- src/frontend/src/components/Admin.vue
+++ src/frontend/src/components/Admin.vue
@@ -1,7 +1,10 @@
 <script setup>
 import { useAuthStore } from '@/stores/auth';
+import { ref} from 'vue';
+import BookItem from './BookItem.vue';
 
 const authStore = useAuthStore();
+const selector = ref("bookList");
 
 async function addBook(){
     try {
@@ -11,16 +14,44 @@ async function addBook(){
         alert('Error adding book: ' + err.message);
     }
 }
+function addBookSelector(){
+    selector.value = 'addBook';
+}
+function listBookSelector(){
+     selector.value = 'bookList';
+}
+function removeBookSelector(){
+    selector.value = 'removeBook';
+}
 </script>
 
 <template>
-    <div class="flex flex-col gap-5">
-        <div class="flex h-12 bg-[#173f5f] justify-center items-center">
+    <div class="flex flex-col gap-5" >
+        <div class="flex h-12 bg-[#173f5f] justify-between items-center">
             <div class="text-[20px] text-white">
                 ADMIN PAGE
             </div>
+            <div class="flex gap-5 mr-2 [&>div]:cursor-pointer">
+            <div @click="addBookSelector()" class="text-[10px] text-white">
+                ADD BOOK
+            </div>
+            <div @click="removeBookSelector()" class="text-[10px] text-white">
+                REMOVE BOOK
+            </div>
+            <div @click="listBookSelector()" class="text-[10px] text-white">
+                LIST BOOK
+            </div>
+            </div>
+
         </div>
-        <div class="flex items-center ml-12 flex-col gap-3 [&>input]:w-1/2 [&>input]:border [&>input]:h-12 [&>input]:rounded-lg [&>input]:pl-6">
+        <div class="flex flex-col gap-5 " v-if="selector==='bookList'">
+              <div class="  [&>*]:rounded-[10px] [&>*]:border [&>*]:border-[#7954bd] " v-for="book in authStore.books" > 
+
+    <BookItem :description="book.description" :title="book.title" :author="book.author" :image="book.image" :published-date="book.publishedDate" :type="book.type" ></BookItem>
+   
+</div>
+        </div >
+        <div class="flex items-center ml-12 flex-col gap-3 [&>input]:w-1/2 [&>input]:border [&>input]:h-12 [&>input]:rounded-lg [&>input]:pl-6" v-else-if="selector === 'addBook'">
             <input v-model="authStore.newBook.title" type="text" placeholder="Title">
             <input v-model="authStore.newBook.author" type="text" placeholder="Author">
             <input v-model="authStore.newBook.image" type="text" placeholder="Image URL">
